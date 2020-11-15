f362d97fdd988b6ec06a4a87fdaa019cd2f5d910
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-11-15 14:07:44 -0700

    JavaScript Projects
    Commenting, Variables, expressions, fontcolor, alerts.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("This site isn't set up yet.") //This is the first line i wrote
+document.write("Text that is showing") //This is the second
+var A = "Using a variable to show this."; //Assigned my first variable 
+document.write(A); //Shown first variable
+
+var person = "Tristan Hillyard" //Var about my name
+window.alert(person) //Printed my name
+
+var quot ="\"I would love to go snowmobiling\" but we need more snow." //Using backslash for quotes and '.
+document.write(quot); //Printed quot variable
+
+document.write("\"Even though there\'s no snow," //Concatenated a string
++" i know that some will fall eventually"
++" -Tristan Hillyard\"")
+
+var B = "\"Twas the night before christmas" //Assigning a variable then concatenating a string
++" not a sound could be heard.\""
+document.write(B); // Printed the variable
+
+var family = "The Hillyards", Dad="Judd", Mom="Tara", Sister1="Tayli", Sister2="Abigial"; //Assigned mulitple variables
+var Dad= Dad.fontcolor("red")
+var Mom= Mom.fontcolor("pink")
+var Sister1= Sister1.fontcolor("Green")
+var Sister2= Sister2.fontcolor("purple")
+document.write(Mom)
+
+document.write(3+4)
