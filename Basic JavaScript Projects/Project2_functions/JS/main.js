f15f552ba688130e3b9340abc30e0bb8c8afc676
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-11-15 15:04:45 -0700

    Functiions
    Change of text and use of a button

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function My_First_Function() {
+    var str = "This text is blue!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result; 
+} //Funtion showing what you will see when you use this function. Assigning Blue_Text as the ID
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("change").innerHTML = sentence;
+} //
+
